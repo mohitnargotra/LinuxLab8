bdb7955c0b33338aa5a1c3bcec852aac7964e9cf
HeadacheG <kingdomneil@gmail.com>
2020-03-26 16:30:28 -0700

    Last

--- myScript.sh
+++ myScript.sh
@@ -4,8 +4,6 @@
 #20W_CST8102 Section 300
 #April 1, 2020
 #This script creates a user interface for user and group management
-
-#variable to track if user is on the menu page or not
 while [ true ]; do
 	echo -en "Choose one of the following options:\nA to Create a user account\nB to Delete a user account\nC to Change supplementary group for a user account\nD to Change initial group for a user account\nE to Change default login shell for a user account\nF to Change account expiration date for a user account\nQ to quit\nWhat would you like to do?: "; read userIn
 	if [ $userIn == A ] || [ $userIn == a ]; then
@@ -43,7 +41,7 @@ while [ true ]; do
 		case $? in
 			0)
 				sleep 3 && clear
-				echo -e "Supplementary group $suppGroup added!\n"
+				echo -e "$userName is now part of $suppGroup!\n"
 				;;
 			*)
 				sleep 3 && clear
@@ -51,7 +49,6 @@ while [ true ]; do
 		esac
 	elif [ $userIn == D ] || [ $userIn == d ]; then
 		echo -en "\nEnter the username of the user you wish to change primary groups for: "; read userName
-		maingrp=$(id -gn $userName)
 		echo -n "Enter the name of the group you'd like to have as your primary group: "; read primGroup
 		sudo usermod -g $primGroup $userName	
 		case $? in
@@ -66,16 +63,17 @@ while [ true ]; do
 	elif [ $userIn == E ] || [ $userIn == e ]; then
 		echo -en "\nEnter the user whos default shell you wish to change: "; read userName
 		echo -n "Enter the directory (absolute path) of the shell: "; read shell
-		sudo usermod -s $shell $userName
+		#compgen is a command to retrieve usernames for all users
+		sudo grep $shell /etc/shells && compgen -u & grep $userName
 		case $? in
-			0)	
+			0)
 				sleep 3 && clear
 				echo -e "Default shell for $userName is now set to $shell\n" 
 				;;
 			*)
 				sleep 3 && clear
 				echo -e "An error occured. The command was unable to finish\n"
-		esac
+			esac
 	elif [ $userIn == F ] || [ $userIn == f ]; then
 		echo -en "\nEnter user whos expiration date you wish to edit: "; read userName
 		echo -n "Enter the expiry date youd like to use (YYYY-MM-DD): "; read expDate 
